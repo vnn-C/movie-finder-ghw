83184ef180bab4e2dd9513a0e4ff173d8525d1a2
vnn-C <108257857+vnn-C@users.noreply.github.com>
2025-06-15 13:33:53 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const searchInput = document.querySelector("searchInput");
+const searchButton = ducument.querySelector("searchButton");
+const resultsContainer = document.querySelector("results");
+
+searchButton.addEventListener("click", () => {
+    const searchTerm = searchInput.ariaValueMax.trim();
+    if(searchTerm){
+        searchMovies(searchTerm);
+    }
+})
+
+const apiKey = "http://www.omdbapi.com/?i=tt3896198&apikey=68303dc9";
+​​const url = `https://www.omdbapi.com/?s=${encodeURIComponent(query)}&apikey=${apiKey}`;
+
+function searchMovies(query){
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {displayMovies(data.Search);})
+        .catch(err => {
+            console.error("API Error", err);
+            resultsContainer.innerHTML = "<p class='text-red-500'>Something went wrong. Please try again.</p>";
+        })
+}
+
+function displayMovies(movies){
+    if (!movies){
+        resultsContainer.innerHTML = "<p class='text-gray-500'>No Results Found</p>";
+        return;
+    }
+
+    resultsContainer.innerHTML = "";
+
+    movies.foreach(movie => {
+        const movieCard = document.createElement("div")
+        movieCard.className = "bg-white rounded shadow p-4 text-left"; 
+        
+        ​​movieCard.innerHTML = ` <h2 class="text-xl font-semibold">${movie.Title}</h2> <p><strong>Year:</strong> ${movie.Year}</p> <img src="${movie.Poster !== "N/A" ? movie.Poster : "https://via.placeholder.com/150"}" alt="Poster for ${movie.Title}" class="mt-2 max-w-xs"> `;
+        resultsContainer.append(movieCard)
+    })
+}
